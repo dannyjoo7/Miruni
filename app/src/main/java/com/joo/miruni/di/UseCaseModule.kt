219144e67817f0ebe17e29d83e3a77f6cb034b25
dannyjoo7 <danny_joo@naver.com>
2024-10-08 17:27:26 +0900

    FEAT : 유스케이스 모듈에 새로운 유스케이스 추가

--- app/src/main/java/com/joo/miruni/di/UseCaseModule.kt
+++ app/src/main/java/com/joo/miruni/di/UseCaseModule.kt
@@ -4,6 +4,8 @@ import com.joo.miruni.domain.usecase.AddTodoItemUseCase
 import com.joo.miruni.domain.usecase.AddTodoItemUseCaseImpl
 import com.joo.miruni.domain.usecase.GetAlarmTimeUseCase
 import com.joo.miruni.domain.usecase.GetAlarmTimeUseCaseImpl
+import com.joo.miruni.domain.usecase.GetTodoItemsForAlarmUseCase
+import com.joo.miruni.domain.usecase.GetTodoItemsForAlarmUseCaseImpl
 import com.joo.miruni.domain.usecase.SaveAlarmTimeUseCase
 import com.joo.miruni.domain.usecase.SaveAlarmTimeUseCaseImpl
 import dagger.Binds
@@ -40,5 +42,16 @@ abstract class UseCaseModule {
     abstract fun bindAddTodoItemUseCase(
         addTodoItemUseCaseImpl: AddTodoItemUseCaseImpl,
     ): AddTodoItemUseCase
+
+    /*
+    * GetTaskItem
+    * */
+
+    // GetTodoTaskItems
+    @Binds
+    @Singleton
+    abstract fun bindGetTodoItemsForAlarmUseCase(
+        getTodoItemsUseCaseImpl: GetTodoItemsForAlarmUseCaseImpl,
+    ): GetTodoItemsForAlarmUseCase
 }
 
