52fc2d8184b805efccdd7de54d7b5355789e73bb
dannyjoo7 <danny_joo@naver.com>
2024-11-21 14:52:14 +0900

    FEAT : 푸시 알람 기능 추가  - AlarmType : 시간대 별 알람 타입 추가

---
+++ app/src/main/java/com/joo/miruni/notifications/AlarmType.kt
@@ -0,0 +1,8 @@
+package com.joo.miruni.notifications
+
+enum class AlarmType(val label: String) {
+    ONE_HOUR_BEFORE("1시간 전"),
+    TEN_MINUTES_BEFORE("10분 전"),
+    FIVE_MINUTES_BEFORE("5분 전"),
+    NOW("지금")
+}
