ec100940b13f94002434221cc82817c409cfdce9
dannyjoo7 <danny_joo@naver.com>
2024-12-03 12:52:20 +0900

    FIX : 필요 없는 import 제거

--- app/src/main/java/com/joo/miruni/presentation/widget/AlarmDisplayDatePicker.kt
+++ app/src/main/java/com/joo/miruni/presentation/widget/AlarmDisplayDatePicker.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,11 +21,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.android.material.timepicker.TimeFormat
 import com.joo.miruni.theme.lightPallet
 import com.ozcanalasalvar.datepicker.ui.theme.PickerTheme
 import com.ozcanalasalvar.datepicker.ui.theme.colorLightPrimary
