8c172b1edb5f31967753413500cb2bf704e68316
dannyjoo7 <danny_joo@naver.com>
2025-01-03 13:55:49 +0900

    FIX : SetupActivity 제거

--- app/src/main/java/com/joo/miruni/presentation/setup/SetupActivity.kt
+++ app/src/main/java/com/joo/miruni/presentation/setup/SetupActivity.kt
@@ -1,7 +1,5 @@
 package com.joo.miruni.presentation.setup
 
-import android.app.AlarmManager
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
@@ -13,35 +11,23 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import com.joo.miruni.presentation.main.MainActivity
+import com.joo.miruni.service.ForegroundService
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SetupActivity : ComponentActivity() {
+
     private val setupViewModel: SetupViewModel by viewModels()
 
     // 알림을 보내기 위한 권한 요청
     private val requestNotificationPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
-                // 권한이 허용되었을 때 모델 초기화
                 initModel()
             } else {
                 // 권한이 거부되었을 때의 처리
                 Toast.makeText(this, "알림 권한이 필요합니다.", Toast.LENGTH_LONG).show()
                 finish()
-            }
-        }
-
-    // TODO 작동이 안됨
-    // 정확한 시간에 알람을 예약하기 위한 권한 요청
-    private val requestExactAlarmPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                // 권한이 허용되었을 때의 처리
-                Toast.makeText(this, "정확한 알람 권한 O", Toast.LENGTH_SHORT).show()
-            } else {
-                // 권한이 거부되었을 때의 처리
-                Toast.makeText(this, "정확한 알람 권한 X", Toast.LENGTH_LONG).show()
             }
         }
 
@@ -54,14 +40,14 @@ class SetupActivity : ComponentActivity() {
         // 권한 요청
         requestPermissions()
 
+        initModel()
+
         setContent {
             SetupScreen(setupViewModel)
         }
     }
 
     private fun requestPermissions() {
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             // POST_NOTIFICATIONS 권한 요청
             if (ContextCompat.checkSelfPermission(
@@ -70,29 +56,31 @@ class SetupActivity : ComponentActivity() {
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 requestNotificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            } else {
-                // API 31 이상
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    if (!alarmManager.canScheduleExactAlarms()) {
-                        requestExactAlarmPermissionLauncher.launch(android.Manifest.permission.SCHEDULE_EXACT_ALARM)
-                    } else {
-                        initModel()
-                    }
-                } else {
-                    // API 30 이하
-                    initModel()
-                }
             }
-        } else {
-            // Android 12 이하
-            initModel()
         }
     }
 
-
     private fun initModel() = with(setupViewModel) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // POST_NOTIFICATIONS 권한 확인
+            if (ContextCompat.checkSelfPermission(
+                    this@SetupActivity,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+            } else {
+                startForegroundService()
+            }
+        } else {
+            startForegroundService()
+        }
+
+
+        // 설정 확인
         isInit.observe(this@SetupActivity) { isInit ->
             if (!isInit) {
+//                startUnlockService()
                 navigateToActivityMainActivity()
             }
         }
@@ -102,5 +90,10 @@ class SetupActivity : ComponentActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun startForegroundService() {
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+        ContextCompat.startForegroundService(this, serviceIntent)
     }
 }
