6632561857bd62a16d16d758a988281369f153ff
dannyjoo7 <danny_joo@naver.com>
2025-01-03 16:36:57 +0900

    FIX : Data 구조 수정  - isPinned 추가

--- app/src/main/java/com/joo/miruni/data/entities/TaskEntity.kt
+++ app/src/main/java/com/joo/miruni/data/entities/TaskEntity.kt
@@ -9,17 +9,24 @@ import java.time.LocalDateTime
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String, // 제목
-    val details: String?, // 세부사항 (null 가능)
+    val details: String?, // 세부사항
 
-    val startDate: LocalDate?, // 시작 날짜 (일정의 경우)
-    val endDate: LocalDate?, // 종료 날짜 (일정의 경우)
+    /*
+    * 일정
+    * */
+    val startDate: LocalDate?, // 시작 날짜
+    val endDate: LocalDate?, // 종료 날짜
 
-    val deadLine: LocalDateTime?, // 마감 날짜 및 시간 (할 일의 경우)
+    /*
+    * 할 일
+    * */
+    val deadLine: LocalDateTime?, // 마감 날짜 및 시간
 
     val alarmDisplayDate: LocalDate?, // 선택된 알람 표시 시간
     val isComplete: Boolean, // 완료 여부
     val completeDate: LocalDateTime?, // 완료 시간
     val type: TaskType, // "SCHEDULE" 또는 "TODO"로 구분할 수 있는 enum
+    val isPinned: Boolean, // 고정 여부
 )
 
 data class TaskItemsEntity(
