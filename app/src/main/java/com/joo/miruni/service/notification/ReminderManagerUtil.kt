e77efd40108cd0d664524d39a2bf61eac432670e
dannyjoo7 <danny_joo@naver.com>
2025-01-03 13:54:46 +0900

    REFACTOR : 알람 초기 설정 부분 기능 리마인더 모듈로 위치 변경

--- app/src/main/java/com/joo/miruni/service/notification/ReminderManagerUtil.kt
+++ app/src/main/java/com/joo/miruni/service/notification/ReminderManagerUtil.kt
@@ -4,7 +4,10 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.joo.miruni.service.ForegroundService
+import java.time.LocalDateTime
+import java.time.ZoneId
 import javax.inject.Inject
 
 class ReminderManagerUtil @Inject constructor(
@@ -14,13 +17,86 @@ class ReminderManagerUtil @Inject constructor(
         const val TAG = "AlarmManagerUtil"
     }
 
-    // 푸쉬 알람 설정
-    fun setExactAlarm(alarmTimeInMillis: Long, id: Int, title: String, reminderType: ReminderType) {
+    // 초기 알람 설정
+    fun setInitAlarm(deadLine: LocalDateTime?, id: Long, title: String) {
+        if (deadLine != null) {
+            val deadLineTime =
+                deadLine.atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli()
+
+            // 현재 시간
+            val nowTime = System.currentTimeMillis()
+
+            // 알람 시간
+            val reminderTime: Long
+
+            // 알람 설정
+            val reminderType: ReminderType = when {
+                nowTime >= deadLineTime -> {
+                    return
+                }
+
+                nowTime < deadLineTime - 3600000 -> {
+                    // 1시간 전 알람 설정
+                    reminderTime = deadLineTime - 3600000
+                    ReminderType.ONE_HOUR_BEFORE
+                }
+
+                nowTime < deadLineTime - 600000 -> {
+                    // 10분 전 알람 설정
+                    reminderTime = deadLineTime - 600000
+                    ReminderType.TEN_MINUTES_BEFORE
+                }
+
+                nowTime < deadLineTime - 300000 -> {
+                    // 5분 전 알람 설정
+                    reminderTime = deadLineTime - 300000
+                    ReminderType.FIVE_MINUTES_BEFORE
+                }
+
+                else -> {
+                    reminderTime = deadLineTime
+                    ReminderType.NOW
+                }
+            }
+
+            Log.d(TAG, "리마인더 날짜 시간 : $reminderTime")
+
+            val intent = Intent(context, ForegroundService::class.java).apply {
+                putExtra("TODO_ID", id)
+                putExtra("TODO_TITLE", title)
+                putExtra("REMINDER_TYPE", reminderType)
+                putExtra("REMINDER_TIME", reminderTime)
+            }
+
+            val pendingIntent = PendingIntent.getService(
+                context,
+                id.hashCode(),
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                reminderTime,
+                pendingIntent
+            )
+        }
+    }
+
+
+    // 다음 알람 설정
+    fun setNextAlarm(
+        reminderTime: Long,
+        id: Long,
+        title: String,
+        reminderType: ReminderType,
+    ) {
         val intent = Intent(context, ForegroundService::class.java).apply {
-            putExtra("TODO_ID", id.toLong())
+            putExtra("TODO_ID", id)
             putExtra("TODO_TITLE", title)
             putExtra("REMINDER_TYPE", reminderType)
-            putExtra("REMINDER_TIME", alarmTimeInMillis)
+            putExtra("REMINDER_TIME", reminderTime)
         }
 
         val pendingIntent = PendingIntent.getService(
@@ -33,7 +109,7 @@ class ReminderManagerUtil @Inject constructor(
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
-            alarmTimeInMillis,
+            reminderTime,
             pendingIntent
         )
     }
